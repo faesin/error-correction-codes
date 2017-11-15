f7f6ac8c22ab5f5bd25ce26e15a8dcfa23aac0db
Rafael Favaro <rafael.faesin@gmail.com>
2017-11-15 17:48:29 -0200

    Finished encoding the message; Started decoding;

--- src/hamming.js
+++ src/hamming.js
@@ -3,32 +3,47 @@
 const encode = function(input, options) {
   if(typeof input !== 'string' || input.match(/[^10]/)) throw new Error('Invalid input. Should be a string binary')
 
-  const codeIndexes = [],
-        encoded = []
+  const encoded = [...input.split('')]
+  let numParityBits = 0
+
+  //Calculate the number of parity bits necessary for the message
+  while(encoded.length > (Math.pow(2, numParityBits) - numParityBits - 1))
+    numParityBits++
+
+  //Create the encoded message with the parity bits  
+  for(let i = 0; i < numParityBits; i++)
+    encoded.splice((Math.pow(2, i) - 1), 0, null)
   
-  for(let i = 1; input.length / i >= 1; i *= 2)
-    codeIndexes.push(i)
+  for(let i = encoded.length; i >= 0; i--) {
+    if(encoded[i] !== null) continue
 
-  if(message.length === 1) codeIndexes.push(2)
+    encoded[i] = '' + encoded.reduce((acc, val, curIndex) => {
+      if(i >= curIndex) return acc
 
-  const getBit = (m, pos) => {
+      // If this bit is covered by the current parity bit, XOR to parity
+      if((curIndex + 1) & (i + 1)) return Number(val)^acc;
 
+      return acc
+    }, 0)
   }
 
-  const getBitInverted = (m, pos) => {
-
-  }
-
-  console.log(message)
-  console.log(codeIndexes)
-
-  codeIndexes.forEach(key => {
-    let bit = getBit(message, key)
-    console.log(`bit at ${key} = ${bit}`)
-  })
-  
+  return encoded.join('')
 }
 
-const decode = function() {
+const decode = function(input, options) {
+  if(typeof input !== 'string' || input.match(/[^10]/)) throw new Error('Invalid input. Should be a string binary')
 
+  let numParityBits = 0
+
+  //Discover how many bits are parity bits
+  while(input.length > (Math.pow(2, numParityBits) - 1))
+    numParityBits++
+
+  const parityBitsPos = []
+  for(let i = 0; i < numParityBits; i++) 
+    parityBitsPos.push(Math.pow(2, i) - 1)
+
+  console.log(parityBitsPos)
+
+  parityBitsPos.forEach()
 }
